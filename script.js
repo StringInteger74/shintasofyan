e89389136fcf48edeee785a4e99d3e2c363d11af
Aldi Aswari <aldiaswari6@gmail.com>
2022-07-08 23:06:40 +0700

    Your commit message

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ footer.innerHTML = sender;
 footer.href = "https://www.instagram.com/aaaaldiaa/";
 
 document.querySelector(".tombol").addEventListener('click', function () {
-  Swal.fire("Hallo Sayangku Dea", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
+  Swal.fire("Hallo Sayangku Ria", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
     Swal.fire("Jawab yang jujur ya!").then(function () {
       Swal.fire("Awas aja kalo boong!!", "", "error").then(function () {
 
